d51cef52c01906c94193f6878bd87581090e5267
Alok Ranjan Meher <root@Aloks-Mac-mini.local>
2016-11-09 22:01:44 +0530

    Date is added

--- src/main/java/com/imos/sample/BasicJava.java
+++ src/main/java/com/imos/sample/BasicJava.java
@@ -1,5 +1,10 @@
+package com.imos.sample;
+
+import java.util.Date;
+
 public class BasicJava {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println(new Date());
   }
 }
