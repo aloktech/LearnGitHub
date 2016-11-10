b3fe767f9ea21eb9614ea354b963aae80482a2d2
aloktech <meher.ranjan@gmail.com>
2016-11-10 07:36:22 +0530

    Time in millis

--- src/main/java/com/imos/sample/BasicJava.java
+++ src/main/java/com/imos/sample/BasicJava.java
@@ -1,10 +1,8 @@
 package com.imos.sample;
 
-import java.util.Date;
-
 public class BasicJava {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Today's : "+new Date());
+    System.out.println("Today's : "+System.currentTimeMillis());
   }
 }
