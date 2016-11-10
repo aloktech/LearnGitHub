9c1cdd6262ef5c7e4d9888ae9cfce7c5ab67f950
aloktech <meher.ranjan@gmail.com>
2016-11-10 07:46:31 +0530

    Time change

--- src/main/java/com/imos/sample/BasicJava.java
+++ src/main/java/com/imos/sample/BasicJava.java
@@ -3,6 +3,6 @@ package com.imos.sample;
 public class BasicJava {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Today's time in milli second : "+System.currentTimeMillis());
+    System.out.println("Today's time in ms : "+System.currentTimeMillis());
   }
 }
